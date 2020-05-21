6a95408c99a549b99611227013048226aef556c1
Peter <Peteremad777@gmail.com>
2020-05-21 14:38:43 +0200

    Initial Files

---
+++ src/main/java/Rooms/RoomsApplication.java
@@ -0,0 +1,13 @@
+package Rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsApplication.class, args);
+	}
+
+}
