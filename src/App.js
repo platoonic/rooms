d07504d32d8e1b47dc259838ca58a7ec28a46546
coderizer <coderizer@aol.com>
2020-05-22 00:42:42 +0200

    Reorganize App.js Structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 //Components
 import Homepage from './components/homepage';
+import Login from './components/login';
+import SignUp from './components/signup';
+//CSS Files
 import './css/ui.css';
 import './css/theme.css';
+//Images
+import Logo from './imgs/logo.png';
 
 function App() {
-  return (
-    <Homepage />
-  );
+	const [loginVisible, showLogin] = useState(0);
+	const [signupVisible, showSignup] = useState(0);
+	return (
+		<div>
+			{loginVisible == 1 &&
+				<Login showLogin = {showLogin}/>
+			}
+			{signupVisible == 1 &&
+				<SignUp showSignup = {showSignup}/>
+			}
+		    <div className="container-md homepage">
+				<div className="row">
+					<div className="col-12">
+						<img src={Logo} alt="rooms" />
+						<Homepage showLogin = {showLogin} showSignup = {showSignup}/>
+					</div>
+				</div>
+			</div>
+		</div>
+  	);
 }
 
 export default App;
