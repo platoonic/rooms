f83c95cef53f79ad5ba4076dce13a7396ba18572
coderizer <coderizer@aol.com>
2020-05-22 00:21:09 +0200

    Add Signup Form

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Logo from '../imgs/logo.png';
 //Components
 import Login from './login';
+import SignUp from './signup';
 
 export default function Homepage(props){
 	const [loginVisible, showLogin] = useState(0);
@@ -12,6 +13,9 @@ export default function Homepage(props){
 			{loginVisible == 1 &&
 				<Login showLogin = {showLogin}/>
 			}
+			{signupVisible == 1 &&
+				<SignUp showSignup = {showSignup}/>
+			}
 			<div className="container-md homepage">
 				<div className="row">
 					<div className="col-12">
@@ -20,7 +24,7 @@ export default function Homepage(props){
 						<p className="description">rooms is a platform that lets people setup private rooms and share their screen/windows/browser tabs or even local 
 						video files with each other and control their playback and more!</p>
 						<div className="action">
-							<a className="button green" href="#">Sign Up</a>
+							<a onClick={() => { showSignup(1) }} className="button green" href="#">Sign Up</a>
 							<span>or</span>
 							<a onClick={() => { showLogin(1) }} className="login" href="#">Login</a>
 						</div>
