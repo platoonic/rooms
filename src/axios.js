6e43c97dd874e00fa09c8777474509859c0cf389
coderizer <coderizer@aol.com>
2020-05-29 00:24:38 +0200

    Restored Heroku base link for frontend HTTP requests

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3000'
+	baseURL: 'https://room-backend-networks.herokuapp.com/'
 });
 
 export default instance;
